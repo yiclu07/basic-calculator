d5904cf13beb1951f9c2ad66d15a73c73b70e546
yiclu07 <yi.c.lu07@gmail.com>
2023-10-27 00:56:30 -0400

    Allow changes to calculator's user input

---
+++ app.js
@@ -0,0 +1,24 @@
+let evalString = "";
+const userInput = document.getElementsByClassName("userInput")[0];
+
+/* regex is set up to look for any character that is not 0-9 or [÷+-×.] */
+const mathRegex = /[^\d÷+\-×\.]|[÷+\-×\.][÷+\-×\.]+/;
+
+const handleChange = (e) => {
+  console.log(`typeof e.target.value: ${typeof e.target.value}`);
+  if (mathRegex.test(e.target.value)) {
+    console.log("if running");
+    console.log(`userInput.value: ${userInput.value}`);
+    console.log(`e.target.value: ${e.target.value}`);
+    userInput.value = evalString;
+  } else {
+    console.log("else running");
+    console.log(`userInput.value: ${userInput.value}`);
+    console.log(`e.target.value: ${e.target.value}`);
+    userInput.value = e.target.value;
+    evalString = e.target.value;
+  }
+  console.log("handleChange is running");
+};
+
+userInput.addEventListener("input", handleChange);
